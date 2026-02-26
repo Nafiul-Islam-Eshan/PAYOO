dc619af52ef61eab0eed528f7c5a1d85fb5dc8f3
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-02-26 15:17:15 +0600

    Working on transactions feature

--- Script/addMoney.js
+++ Script/addMoney.js
@@ -15,70 +15,67 @@
  *    5-2- false:: show an error alert
  */
 
+document.getElementById("btn-add-money").addEventListener("click", function () {
+  // 1- Get the bank
+  const addMoneyBank = giveIdTakeInputValue("add-money-bank");
+  if (addMoneyBank === "Select A Bank") {
+    alert("Please, select a bank");
+    return;
+  }
 
-document.getElementById("btn-add-money").addEventListener("click", function() { 
-    // 1- Get the bank
-    const addMoneyBank = giveIdTakeInputValue("add-money-bank");
-    if(addMoneyBank === "Select A Bank"){
-        alert('Please, select a bank');
-        return;
-    }
-    
-    // 2- get the account Number
-    const accountNumber = giveIdTakeInputValue("add-money-number")
-    // 2-1- account number validaton
-    if(accountNumber.length !== 11){
-        alert("Invalid account number");
-        return;
-    }
-    
-    // 3- Get the add money ammount
-    const addMoneyAmmount = giveIdTakeInputValue("add-money-amount");
-    
-    // 4- New balance calculation
-    const currentBalance = getBalance();
-    const newBalance = currentBalance + Number(addMoneyAmmount);
+  // 2- get the account Number
+  const accountNumber = giveIdTakeInputValue("add-money-number");
+  // 2-1- account number validaton
+  if (accountNumber.length !== 11) {
+    alert("Invalid account number");
+    return;
+  }
 
-    // 5- get the password and check
-    const addMoneyPassword = giveIdTakeInputValue("add-money-password")
+  // 3- Get the add money ammount
+  const addMoneyAmmount = giveIdTakeInputValue("add-money-amount");
 
-    // 5-1 true:: show an alert and set new balance
-    if (addMoneyPassword == "1234"){
-        const d = new Date()
-        const date = d.toLocaleDateString("en-GB");
-        const time = d.toLocaleTimeString("en-GB");
+  // 4- New balance calculation
+  const currentBalance = getBalance();
+  const newBalance = currentBalance + Number(addMoneyAmmount);
 
-        alert(`Add money successful at ${time} on ${date} ✅`);
-        setBalance(newBalance);
+  // 5- get the password and check
+  const addMoneyPassword = giveIdTakeInputValue("add-money-password");
 
-        // 5-1-1- save the transaction history to transactions
-        // 5-1-2- get the transitions-cards-container
-        const parent = document.getElementById("transitions-cards-container");
-        
-        // 5-1-3- create new div
-        const newDiv = document.createElement("div");
-        
-        // 5-1-4- inserting infos into the div by changing innerHTMl
-        newDiv.innerHTML = `
+  // 5-1 true:: show an alert and set new balance
+  if (addMoneyPassword == "1234") {
+    const d = new Date();
+    const date = d.toLocaleDateString("en-GB");
+    const time = d.toLocaleTimeString("en-GB");
+
+    alert(`Add money successful at ${time} on ${date} ✅`);
+    setBalance(newBalance);
+
+    // 5-1-1- save the transaction history to transactions
+    // 5-1-2- get the transitions-cards-container
+    const parent = document.getElementById("transitions-cards-container");
+
+    // 5-1-3- create new div
+    const newDiv = document.createElement("div");
+
+    // 5-1-4- inserting infos into the div by changing innerHTMl
+    newDiv.innerHTML = `
         <div class="p-5 bg-base-200 ">
             <img src="./assets/opt-1.png" alt="">
             <p class="font-bold text-xl">Add Money</p>
             Account Number = ${accountNumber} <br>
-            Amount = ${addMoneyAmmount} Taka <br>
+            Amount = <span class="text-green-500"> +${addMoneyAmmount} Taka</span> <br>
             Time = ${time} <br>
             Date = ${date} <br>
         </div>
         `;
 
-        // 5-1-5- append the new div to the transitions-cards-container
-        parent.append(newDiv);
+    // 5-1-5- append the new div to the transitions-cards-container
+    parent.append(newDiv);
+  }
 
-    }
-
-    // 5-2 false:: show an error alert
-    else{
-        alert( `Invalid Password`);
-        return;
-    }
-
+  // 5-2 false:: show an error alert
+  else {
+    alert(`Invalid Password`);
+    return;
+  }
 });
--- Script/home.js
+++ Script/home.js
@@ -7,65 +7,89 @@
        4-1- New balance validation
     5- Get the password and verify
        5-1 true:: show an alert and set new balance
+           5-1-1- save the transaction history to transactions
+           5-1-2- get the transitions-cards-container
+           5-1-3- create new div
+           5-1-4- inserting infos into the div by changing innerHTMl
+           5-1-5- append the new div to the transitions-cards-container 
        5-2 false:: show an error alert
  */
 
-document.querySelector("#btn-cashout").addEventListener("click",function(event){
+document
+  .querySelector("#btn-cashout")
+  .addEventListener("click", function (event) {
     // 1- Get the agent number and validate
     const agentNumber = giveIdTakeInputValue("cashout-number");
-    console.log("Agent Number: ",agentNumber);
-    if(agentNumber.length !== 11 ){
-        alert("Invalid Phone Number");
-        return;
+    if (agentNumber.length !== 11) {
+      alert("Invalid Phone Number");
+      return;
     }
 
     // 2- Get the amount, validate, convert to number
-    const casoutAmount = Number(giveIdTakeInputValue("cashout-amount")) ;
-    console.log("Cash out amount:",casoutAmount);
+    const casoutAmount = Number(giveIdTakeInputValue("cashout-amount"));
 
     // 3- Get the current balance
     balance = getBalance();
-    console.log("Balance:",balance)
 
     // 4- Calculate new balance
     let newBalance = Number(balance) - Number(casoutAmount);
     // 4-1- New balance validation
-    if (newBalance < 0){
-        alert("You do not have sufficient balance");
-        return;
-    } 
+    if (newBalance < 0) {
+      alert("You do not have sufficient balance");
+      return;
+    }
 
     // 5- Get the password and verify
-    const cashoutPassword = giveIdTakeInputValue("cashout-password") ;
-    console.log("Password:",cashoutPassword);
-     
-    // 5-1 true:: show an alert and set new balance
-    if (cashoutPassword == "1234"){
-        const d = new Date()
-        const date = d.toLocaleDateString("en-GB");
-        const time = d.toLocaleTimeString("en-GB");
+    const cashoutPassword = giveIdTakeInputValue("cashout-password");
+    console.log("Password:", cashoutPassword);
 
-        alert(`Cashout successful at ${time} on ${date} ✅`);
-        setBalance(newBalance);
-        console.log("New Balance:",newBalance);
+    // 5-1 true:: show an alert and set new balance
+    if (cashoutPassword == "1234") {
+      const d = new Date();
+      const date = d.toLocaleDateString("en-GB");
+      const time = d.toLocaleTimeString("en-GB");
+
+      alert(`Cashout successful at ${time} on ${date} ✅`);
+      setBalance(newBalance);
+
+      // 5-1-1- save the transaction history to transactions
+      // 5-1-2- get the transitions-cards-container
+      const parent = document.getElementById("transitions-cards-container");
+
+      // 5-1-3- create new div
+      const newDiv = document.createElement("div");
+
+      // 5-1-4- inserting infos into the div by changing innerHTMl
+      newDiv.innerHTML = `
+        <div class="p-5 bg-base-200 ">
+            <img src="./assets/opt-2.png" alt="">
+            <p class="font-bold text-xl">Cash out</p>
+            Agent Number = ${agentNumber} <br>
+            Amount = <span class="text-red-500"> -${casoutAmount} Taka</span><br>
+            Time = ${time}<br>
+            Date = ${date}<br>
+        </div>
+        `;
+
+      // 5-1-5- append the new div to the transitions-cards-container
+      parent.append(newDiv);
     }
 
     // 5-2 false:: show an error alert
-    else{
-        alert("Invalid Password");
+    else {
+      alert("Invalid Password");
     }
-});
+  });
 
 // Hide all forms just when browser is loaded.
-document.addEventListener("DOMContentLoaded", function(){
-    const addMoney = document.getElementById("add-money");
-    const cashout = document.getElementById("cashout");
-    const transferMoney = document.getElementById("transfer-money");
-    const transactions = document.getElementById("transactions");
+document.addEventListener("DOMContentLoaded", function () {
+  const addMoney = document.getElementById("add-money");
+  const cashout = document.getElementById("cashout");
+  const transferMoney = document.getElementById("transfer-money");
+  const transactions = document.getElementById("transactions");
 
-    addMoney.classList.add("hidden");
-    cashout.classList.add("hidden");
-    transferMoney.classList.add("hidden");
-    transactions.classList.add("hidden");
-
-})
+  addMoney.classList.add("hidden");
+  cashout.classList.add("hidden");
+  transferMoney.classList.add("hidden");
+  transactions.classList.add("hidden");
+});
